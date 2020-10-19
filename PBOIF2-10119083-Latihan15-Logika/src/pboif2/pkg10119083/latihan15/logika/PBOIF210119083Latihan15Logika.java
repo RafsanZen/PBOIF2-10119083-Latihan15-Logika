e23f212114369e3e61b55f417eec6393f8d92b88
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-10-19 19:30:25 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan15-Logika/src/pboif2/pkg10119083/latihan15/logika/PBOIF210119083Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * Nama      : Rafsan Zen Mustaofa
+ * Kelas     : IF2
+ * NIM       : 10119083
+ * Deskripsi : logika
+ */
+public class PBOIF210119083Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a  b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("! (a && b) = " + ! (a && b));
+    }
+    
+}
